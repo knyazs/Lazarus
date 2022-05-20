30110d6a48745702ce6f3cefeea43e5210198a1c
Miljan Radovic <miljan.radovic@gmail.com>
2022-05-20 22:26:34 +0400

    Added more tables

--- MasterDb/Tables/Dataset.sql
+++ MasterDb/Tables/Dataset.sql
@@ -6,6 +6,8 @@ CREATE TABLE [dbo].[Dataset]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[DomainId] INT NOT NULL,
+	[LocationId] SMALLINT NOT NULL,
 	[Name] VARCHAR(100) NOT NULL, 
-    CONSTRAINT [FK_Dataset_to_Domain] FOREIGN KEY ([DomainId]) REFERENCES [dbo].[Domain]([Id]),
+    CONSTRAINT [FK_Dataset_to_Domain] FOREIGN KEY ([DomainId]) REFERENCES [dbo].[Domain]([Id]), 
+    CONSTRAINT [FK_Dataset_to_Location] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[Location]([Id]),
 )
---
+++ MasterDb/Tables/Location.sql
@@ -0,0 +1,9 @@
+﻿-- LOCATION
+--
+-- Geo location of data center
+CREATE TABLE [dbo].[Location]
+(
+	[Id] SMALLINT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Code] VARCHAR(100) NOT NULL,
+	[Name] VARCHAR(255) NOT NULL,
+)
